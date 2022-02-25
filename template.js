416e382d83921d9f157621b902e8b1329a233cf4
Usenmfon <usenmfonuko@gmail.com>
2022-02-25 05:23:08 -0800

    initial commit

---
+++ template.js
@@ -0,0 +1,10 @@
+console.log("Starting....");
+
+const NETWORKS = {
+    mtn: [0803, 0904, 0701],
+    airtel: [0901, 0801, 0900]
+}
+
+console.log('mtn numbers ===> ', NETWORKS.airtel.lastIndexOf(0900))
+
+
