59c0eadee553eb5fd381109d739c08ad58b0b0a0
Usenmfon <usenmfonuko@gmail.com>
2022-03-06 22:05:21 -0800

    added speech synthesis

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-var SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-var SpeechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList
-var SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+let SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+let SpeechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList
+let SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
-var mtnnumbers = ['blue', '0806', '0703'];
-var grammar = '#JSF V1.0; grammar mtnnumbers; public <mtnnumbers> = ' + mtnnumbers.join(' | ') + ' ;'
+const diagnostic = document.querySelector('.audio')
+const microphone = document.querySelector('.fa-microphone')
+const network_name = document.querySelector('.network_name')
+const error_message = document.querySelector('.error')
+const search = document.getElementById("search");
 
-var recognition = new SpeechRecognition();
-var speechRecognitionList = new SpeechGrammarList();
+let synth = window.speechSynthesis;
+let voices = [];
+
+const numbers = ['0706', '0708', '0806','0812', '0703', '0808', '0805','0807','0705'];
+const grammar = '#JSF V1.0; grammar numbers; public <numbers> = ' + numbers.join(' | ') + ' ;'
+
+let recognition = new SpeechRecognition();
+let speechRecognitionList = new SpeechGrammarList();
 
 speechRecognitionList.addFromString(grammar, 1);
 
@@ -16,20 +25,30 @@ recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1; 
 
-var diagnostic = document.querySelector('.microphone')
-var networkname = document.querySelector('.network_name')
+error_message.textContent = "Say the first 4 digits"
 
-diagnostic.addEventListener('click', ()=> {
+diagnostic.addEventListener('click', ()=> { 
   console.log('clicked')
+  microphone.classList.add("audio_acive")
   recognition.start();
   console.log("Ready to receive a command.")
 })
 
 recognition.onresult = function(event) {
-  var network = event.results[0][0].transcript;
-  console.log("Result received ==> " , network)
-  networkname.textContent = network;
-  console.log('Confidence: ' + event.results[0][0].confidence)
+  let network = event.results[0][0].transcript;
+  let value = handleNetwork(network)
+  sSynthesis(value)
+  // console.log('Confidence: ' + event.results[0][0].confidence)
+}
+
+function sSynthesis(netwok){
+  voices = synth.getVoices();
+  let utterThis = new SpeechSynthesisUtterance(netwok)
+  utterThis.voice = voices[2]
+  utterThis.pitch = 1;
+  utterThis.rate = 1;
+  synth.speak(utterThis);
+  microphone.classList.remove("audio_acive")
 }
 
 recognition.onspeechend = function(){
@@ -41,58 +60,30 @@ recognition.onnomatch = function(event){
 }
 
 recognition.onerror = function(event){
-  
-  console.log("error ===>", event.error)
+  error_message.textContent = event.error 
 }
 
+search.addEventListener("input", (e) => {
+  let value = e.target.value;
+  if(value === ""){
+    network_name.textContent = "Network";
+  }
+  handleNetwork(value.slice(0,4));
+});
 
 
-// const mtn_numbers = document.getElementById("mtn_numbers");
-// const airtel_numbers = document.getElementById("airtel_numbers");
-// const glo_numbers = document.getElementById("glo_numbers");
-// const nine_mobile_numbers = document.getElementById("nine_mobile_numbers");
-// const search = document.getElementById("search");
-// const network_name = document.getElementsByClassName("network_name")[0];
-
-// search.addEventListener("input", (e) => {
-//   let value = e.target.value;
-//   if (['0803', '0806', '0703', '0706', '0813', '0816', '0810', '0814', '0903'].includes(value)) {
-//     network_name.innerHTML = "MTN";
-//   }
-//   if (['0802', '0808', '0708', '0812', '0701', '0902',].includes(value)) {
-//     network_name.innerHTML = "AIRTEL";
-//   }
-//   if (['0805','0807','0705','0815','0811','0905',].includes(value)) {
-//     network_name.innerHTML = "GLO";
-//   }
-//   if (['0809','0818','0817','0909'].includes(value)) {
-//     network_name.innerHTML = "9MOBILE";
-//   }
-// });
-
-// const handleClick = (network) => {
-//   switch (network) {
-//     case "MTN":
-//       changeDisplay(mtn_numbers);
-//       break;
-//     case "AIRTEL":
-//       changeDisplay(airtel_numbers);
-//       break;
-//     case "GLO":
-//       changeDisplay(glo_numbers);
-//       break;
-//     case "NINEMOBILE":
-//       changeDisplay(nine_mobile_numbers);
-//       break;
-//     default:
-//       break;
-//   }
-// };
-
-// function changeDisplay(network) {
-//   if (network.style.display === "block") {
-//     network.style.display = "none";
-//   } else {
-//     network.style.display = "block";
-//   }
-// }
+function handleNetwork(network) {
+  if (['0803', '0806', '0703', '0706', '0813', '0816', '0810', '0814', '0903'].includes(network)) {
+    network_name.textContent = "MTN";
+  }
+  if (['0802', '0808', '0708', '0812', '0701', '0902',].includes(network)) {
+    network_name.textContent = "AIRTEL";
+}
+  if (['0805','0807','0705','0815','0811','0905',].includes(network)) {
+    network_name.textContent = "GLO";
+  }
+  if (['0809','0818','0817','0909'].includes(network)) {
+    network_name.textContent = "9MOBILE";
+  }
+  return network_name.textContent
+}
